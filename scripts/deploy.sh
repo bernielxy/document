7b75e61bef85dcf9af688def842bd87699b8cee4
sharkspeed <xiangyuliu2016@outlook.com>
2019-01-22 18:05:20 +0800

    add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+openssl aes-256-cbc -K $encrypted_1c183dd38b6b_key -iv $encrypted_1c183dd38b6b_iv -in travis_rsa.enc -out travis_rsa -d
+chmod 600 travis_rsa
+cp travis_rsa ~/.ssh/id_rsa
+
+rsync -avz -e "ssh -i ./.ssh/id_rsa" document/raneto/content ubuntu@118.24.211.230:/home/ubuntu/workplace/ss-doc/content
