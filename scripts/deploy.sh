83de55f338fb71ea892b77927cf0b008e8e52759
sharkspeed <xiangyuliu2016@outlook.com>
2019-01-22 18:15:17 +0800

    skip check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ openssl aes-256-cbc -K $encrypted_1c183dd38b6b_key -iv $encrypted_1c183dd38b6b_i
 chmod 600 travis_rsa
 cp travis_rsa ~/.ssh/id_rsa
 
-rsync -avz -e "ssh -i ./.ssh/id_rsa" document/raneto/content ubuntu@118.24.211.230:/home/ubuntu/workplace/ss-doc/content
+rsync -avz -e "ssh -o 'StrictHostKeyChecking no' -i ~/.ssh/id_rsa" document/raneto/content ubuntu@118.24.211.230:/home/ubuntu/workplace/ss-doc/content
